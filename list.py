8d0a7df79ee3c5f9200944fe4c6f56f8228dd9ef
Tomas Toledo <tomas_toledo98@hotmail.com>
2024-11-19 20:32:59 -0300

    listas agregadas

---
+++ list.py
@@ -0,0 +1,75 @@
+# thislist = ["apple", "banana", "cherry"]
+# if "apple" in thislist:
+#   print("Yes, 'apple' is in the fruits list")
+
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("mango")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# otherlist = ["tomato", "mango"]
+# thislist.extend(otherlist)
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop()
+# print(thislist) 
+
+# thislist.clear()
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#   print(x)
+
+
+# newlist = [expression for item in iterable if condition == True]
+# newlist = [x for x in fruits if x != "apple"]
+
+
+# thislist = ["banana", "apple", "kiwi", "mango"]
+
+# thislist.sort()
+# print(thislist)
+
+
+# thislist = [10, 50, 25, 100, 75]
+
+# thislist.sort(reverse=True)
+# print(thislist)
+
+# tecnologias = ["Angular", "React", "Node", "Express"]
+
+# for tecnologia in tecnologias:
+#     print (tecnologia)
+
+# [print(tecnologia) for tecnologia in tecnologias]
+
+# listaConA = [tecnologia for tecnologia in tecnologias if "a" in tecnologia]
+
+# print(listaConA)
+
+# for i in range(len(tecnologias)):
+#     print(f"{i+1}.{tecnologias[i]}")
+
+# def myfunc(n):
+#   return abs(n - 50)
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(key = myfunc)
+# print(thislist)
+
+# usuarios = [["Usuario 1", 1],["Usuario 2", 2],["Usuario 3", 3]]
+# usuarios.sort(key = lambda el: el[1], reverse=True)
+# print(usuarios)
+
